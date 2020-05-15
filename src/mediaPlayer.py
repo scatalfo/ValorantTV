9d09ef6e612cefec68dfb5f29ac7780a994e8b35
scatalfo <scatalfo@gmail.com>
2020-05-15 16:00:10 -0400

    Pause Button
    -Added Pause Button (doesn't work yet)
    -Modified Dimensions
    -Added assets for pause button
    -Modified GUI transitions to properly switch from video mode to needs a video mod

--- src/mediaPlayer.py
+++ src/mediaPlayer.py
@@ -1,15 +1,15 @@
 import pyglet
+from pyglet import image
 from pyglet.window import FPSDisplay
 import glooey
 
 pyglet.options['search_local_libs'] = True
 
 print(pyglet.media.have_ffmpeg())
-window= pyglet.window.Window(fullscreen=False, width = 1280, height = 720, file_drops=True, caption="ValorantTV")
+window= pyglet.window.Window(fullscreen=False, width = 1280, height = 720+64, file_drops=True, caption="ValorantTV")
 
 fps_display = FPSDisplay(window)
 player = pyglet.media.Player()
-#filename = "resources/scarra.mp4"
 
 
 
@@ -17,24 +17,42 @@ player = pyglet.media.Player()
 def on_draw():
 
     if player.source and player.source.video_format:
-        player.get_texture().blit(0,0, height = 720, width = 1280)
+        player.get_texture().blit(0,64, height = 720, width = 1280)
         fps_display.draw()
-    else:
-        #print('hi')
-        gui = glooey.Gui(window)
-        label = glooey.Label('Drop your video to get started!')
-
+    elif(gui.__contains__(hbox)):
+        gui.remove(hbox)
         gui.add(label)
 
 @window.event
 def on_file_drop(x, y, path):    
-    print(x, y, path)
     filename = path[0]
     src = pyglet.media.load(filename, decoder = pyglet.media.codecs.ffmpeg.FFmpegDecoder())
     player.queue(src)
     player.play()
+    gui.remove(label)
+    gui.add(hbox)
 
+#GUI STUFF GOES HERE
+class PauseButton(glooey.Checkbox):
+    playImage = image.load('resources/play_64.png')
+    pauseImage = image.load('resources/pause_64.png')
+    custom_checked_base = pauseImage
+    custom_checked_over = pauseImage
+    custom_checked_down = pauseImage
+    custom_unchecked_base = playImage
+    custom_unchecked_over = playImage
+    custom_unchecked_down = playImage
+    custom_alignment = 'bottom left'
 
+gui = glooey.Gui(window)
+pauseToggle = PauseButton()
+label = glooey.Label('Drop your video to get started!')
+label.set_font_size(50)
+label.set_color('white')
+hbox = glooey.HBox()
 
+gui.add(label)
+hbox.add(pauseToggle)
 
+#gui.add(hbox)
 pyglet.app.run()
